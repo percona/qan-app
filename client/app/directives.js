b34c8cd603ac2c49aa611f56fae2b19bec77c649
Andrii Skomorokhov <andrii.skomorokhov@percona.com>
2016-06-16 15:16:32 +0000

    [PPL-209] Added sparklines to Load and Count columns at top 10 queries tables.

--- client/app/directives.js
+++ client/app/directives.js
@@ -36,158 +36,310 @@
     });
 
     /**
-     * @desc load bar chart
-     * @example <load-bar></load-bar>
+     * @desc load sparklines
+     * @example <load-sparklines></load-sparklines>
      */
-    pplDirectives.directive('loadBar',  function ($parse) {
+    pplDirectives.directive('loadSparklines',  function ($parse) {
         return {
             restrict: 'E',
-            // this is important,
-            // we don't want to overwrite our directive declaration
-            // in the HTML mark-up
             replace: false,
             scope: {data: '=chartData'},
             link: function(scope, element, attrs) {
-                var chart = d3.select(element[0]);
-                chart.append('div')
-                    .attr('class', 'chart')
-                    .style('width', function() {return '100%'})
-                    .selectAll('div')
-                    .data(scope.data)
-                    .enter()
-                    .append('div')
-                    .transition().ease('elastic')
-                    .style('width', function(d) { return d.gtt + '%'; })
-                    .text(function(d) {
-                         var text = d.load < 0.01 ? '<0.01' : d.load;
-                         text += ' (' + d.gtt + '%)';
-                         return text;
-                    });
-            }
-        };
-    });
 
-    /**
-     * @desc count bar chart
-     * @example <count-bar></count-bar>
-     */
-    pplDirectives.directive('countBar',  function ($parse) {
-        return {
-            restrict: 'E',
-            replace: false,
-            scope: {data: '=chartData'},
-            link: function(scope, element, attrs) {
-                var chart = d3.select(element[0]);
-                chart.append('div')
-                    .attr('class', 'chart')
-                    .style('width', function() {return '100%'})
-                    .selectAll('div')
-                    .data(scope.data)
-                    .enter()
-                    .append('div')
-                    .transition().ease('elastic')
-                    .style('width', function(d) { return d.percentage + '%'; })
-                    .text(function(d) {
-                         var text = d.qps < 0.01 ? '<0.01' : d.qps;
-                         text += ' (' + d.percentage + '%)';
-                         return text;
-                    });
-            }
-        };
-    });
+                var iso = d3.time.format.utc('%Y-%m-%dT%H:%M:%SZ');
 
-    /**
-     * @desc latency box chart
-     * @example <latency-box></latency-box>
-     */
-    pplDirectives.directive('latencyBox', function ($parse) {
-        return {
-            restrict: 'E',
-            replace: false,
-            scope: {data: '=chartData'},
-            link: function(scope, element, attrs) {
                 var chart = d3.select(element[0]);
-                var svgContainer = chart.append('svg')
-                    .attr('class', 'latency-box')
-                    .attr("tooltip-append-to-body", true)
-                    .attr("tooltip", function(d){
-                        return 'Hello World!' + (scope.data.row === null ? 'bebe' : scope.data.row.Max);
+                var svg = chart.append('svg')
+                    .attr('class', 'load-sparklines');
+
+                    var margin = {
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        bottom: 0
+                    };
+
+                    var height = 15 - margin.top - margin.bottom;
+                    var width = 200 - margin.left - margin.right;
+
+                    var xDomain = d3.extent(scope.data, function(d) {
+                        return iso.parse(d.Start_ts);
                     })
-                    .append('g');
+                    var yDomain = d3.extent(scope.data, function(d) {
+                        return d.Query_time_sum;
+                    });
 
-                var maxX = function () {
-                    if (scope.data.row === null) {
-                        return '100%';
-                    } else {
-                        return (scope.data.row.Max / scope.data.total.Max * 100).toFixed(0) + '%';
-                    }
-                }();
+                    var xScale = d3.time.scale().range([0, width]).domain(xDomain);
+                    var xScale2 = d3.time.scale().range([0, 1]).domain(xDomain);
+                    var yScale = d3.scale.linear().range([height, 0]).domain(yDomain);
 
-                var posX = function(metricName) {
-                    if(scope.data.row === null) {
-                        return (scope.data.total[metricName] / scope.data.total.Max * 100).toFixed(0) + '%';
-                    } else {
-                        return (scope.data.row[metricName] / scope.data.total.Max * 100).toFixed(0) + '%' ;
-                    }
-                };
+                    var line = d3.svg.line()
+                        .x(function(d) {
+                            return xScale(iso.parse(d.Start_ts));
+                        })
+                        .y(function(d) {
+                            return yScale(d.Query_time_sum);
+                    });
 
-                var minX = posX('Min');
+                    var area = d3.svg.area()
+                        .x(function(d) {
+                            return xScale(iso.parse(d.Start_ts));
+                        })
+                        .y0(function(d) {
+                            return yScale(d.Query_time_sum);
+                        })
+                        .y1(height);
 
-                // Draw the lines
-                var hrLine = svgContainer.append("line")
-                    .attr('class', 'latency-box-line')
-                    .attr('x1', '0%')
-                    .attr('y1', '9px')
-                    .attr('x2', '100%')
-                    .attr('y2', '9px');
+                    var g = svg.append('g').attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
 
-                var hrLine = svgContainer.append("line")
-                    .attr('class', 'latency-box-values')
-                    .attr('x1', minX)
-                    .attr('y1', '9px')
-                    .attr('x2', maxX)
-                    .attr('y2', '9px');
+                    g.append('path')
+                        .datum(scope.data)
+                        .attr('class', 'area')
+                        .attr('d', area);
 
-                var minLine = svgContainer.append("line")
-                    .attr('class', 'latency-box-min')
-                    .attr('x1', minX)
-                    .attr('y1', '2px')
-                    .attr('x2', minX)
-                    .attr('y2', '15px');
 
-                var maxLine = svgContainer.append("line")
-                    .attr('class', 'latency-box-max')
-                    .attr('x1', maxX)
-                    .attr('y1', '2px')
-                    .attr('x2', maxX)
-                    .attr('y2', '15px');
+                    g.append('path')
+                        .datum(scope.data)
+                        .attr('class', 'line')
+                        .attr('d', line);
 
-                var avgX = posX('Avg');
-                var avgLine = svgContainer.append("line")
-                    .attr('class', 'latency-box-avg')
-                    .attr('x1', avgX)
-                    .attr('y1', '2px')
-                    .attr('x2', avgX)
-                    .attr('y2', '15px')
-                    .attr('stroke-dasharray',  '1,1');
+                    // focus tracking
+                    // TODO: cleanup
 
-                var medX = posX('Med');
-                var medLine = svgContainer.append("line")
-                    .attr('class', 'latency-box-med')
-                    .attr('x1', medX)
-                    .attr('y1', '2px')
-                    .attr('x2', medX)
-                    .attr('y2', '15px')
+                    var focus = g.append('g').style('display', 'none');
 
-                var p95X = posX('P95');
-                var p95Line = svgContainer.append("line")
-                    .attr('class', 'latency-box-p95')
-                    .attr('x1', p95X)
-                    .attr('y1', '2px')
-                    .attr('x2', p95X)
-                    .attr('y2', '15px');
+                    focus.append('line')
+                        .attr('id', 'focusLineX')
+                        .attr('class', 'focusLine');
+
+                    /*
+                    focus.append('line')
+                        .attr('id', 'focusLineY')
+                        .attr('class', 'focusLine');
+                    */
+
+                    focus.append('circle')
+                        .attr('id', 'focusCircle')
+                        .attr('r', 1)
+                        .attr('class', 'circle focusCircle');
+
+                    /*
+                    focus.append("text")
+                        .attr('id', 'focusTexty')
+                        .attr("x", 9)
+                        .attr("dy", ".1em");
+                    focus.append("text")
+                        .attr('id', 'focusTextx')
+                        .attr("x", 40)
+                        .attr("dy", ".1em");
+                    */
+
+                    var bisectDate = d3.bisector(function(d) {
+                        return iso.parse(d.Start_ts);
+                    }).left;
+
+                    g.append('rect')
+                        .attr('class', 'overlay')
+                        .attr('width', width)
+                        .attr('height', height)
+                        .on('mouseover', function() {
+                            focus.style('display', null);
+                        })
+                    .on('mouseout', function() {
+                        focus.style('display', 'none');
+                    })
+                    .on('mousemove', function() {
+                        var mouse = d3.mouse(this);
+                        var mouseDate = xScale.invert(mouse[0]);
+                        var i = bisectDate(scope.data, mouseDate) || 1; // returns the index to the current data item
+                        var d0 = scope.data[i-1]
+                        var d1 = scope.data[i];
+                        // work out which date value is closest to the mouse
+                        var d = mouseDate - iso.parse(d0.Start_ts) > iso.parse(d1.Start_ts) - mouseDate ? d1 : d0;
+
+                        var x = xScale(iso.parse(d.Start_ts));
+                        var y = yScale(d.Query_time_sum);
+
+                        var MIN = 0,
+                        MAX = 1;
+                        focus.select('#focusCircle')
+                            .attr('cx', x)
+                            .attr('cy', y);
+                        focus.select('#focusLineX')
+                            .attr('x1', x).attr('y1', yScale(yDomain[MIN]))
+                            .attr('x2', x).attr('y2', yScale(yDomain[MAX]));
+                        /*
+                        focus.select('#focusLineY')
+                            .attr('x1', xScale(xDomain[MIN])).attr('y1', y)
+                            .attr('x2', xScale(xDomain[MAX])).attr('y2', y);
+                        focus.select("#focusTexty")
+                            .attr("transform", "translate(" + xScale(xDomain[MIN]) + "," + (y + 3) + ")")
+                            // you may want to format the value here
+                            .text(d.Query_time_sum);
+                        x = x - focus.select("#focusTextx").node().getComputedTextLength() * xScale2(iso.parse(d.Start_ts));
+                        focus.select("#focusTextx")
+                            .attr("transform", "translate(" + x + "," + (yScale(yDomain[MAX]) + 3) + ")")
+                            // you may want to format the value here
+                            .text(iso.parse(d.Start_ts));
+                            */
+                    });
+
             }
         };
     });
+
+    /**
+     * @desc count sparklines
+     * @example <count-sparklines></count-sparklines>
+     */
+    pplDirectives.directive('countSparklines',  function ($parse) {
+        return {
+            restrict: 'E',
+            replace: false,
+            scope: {data: '=chartData'},
+            link: function(scope, element, attrs) {
+
+                var iso = d3.time.format.utc('%Y-%m-%dT%H:%M:%SZ');
+
+                var chart = d3.select(element[0]);
+                var svg = chart.append('svg')
+                    .attr('class', 'load-sparklines');
+
+                    var margin = {
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        bottom: 0
+                    };
+
+                    var height = 15 - margin.top - margin.bottom;
+                    var width = 200 - margin.left - margin.right;
+
+                    var xDomain = d3.extent(scope.data, function(d) {
+                        return iso.parse(d.Start_ts);
+                    })
+                    var yDomain = d3.extent(scope.data, function(d) {
+                        return d.Query_count;
+                    });
+
+                    var xScale = d3.time.scale().range([0, width]).domain(xDomain);
+                    var xScale2 = d3.time.scale().range([0, 1]).domain(xDomain);
+                    var yScale = d3.scale.linear().range([height, 0]).domain(yDomain);
+
+                    var line = d3.svg.line()
+                        .x(function(d) {
+                            return xScale(iso.parse(d.Start_ts));
+                        })
+                        .y(function(d) {
+                            return yScale(d.Query_count);
+                    });
+
+                    var area = d3.svg.area()
+                        .x(function(d) {
+                            return xScale(iso.parse(d.Start_ts));
+                        })
+                        .y0(function(d) {
+                            return yScale(d.Query_count);
+                        })
+                        .y1(height);
+
+                    var g = svg.append('g').attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+
+                    g.append('path')
+                        .datum(scope.data)
+                        .attr('class', 'area')
+                        .attr('d', area);
+
+
+                    g.append('path')
+                        .datum(scope.data)
+                        .attr('class', 'line')
+                        .attr('d', line);
+
+                    // focus tracking
+
+                    var focus = g.append('g').style('display', 'none');
+
+                    focus.append('line')
+                        .attr('id', 'focusLineX')
+                        .attr('class', 'focusLine');
+
+                    /*
+                    focus.append('line')
+                        .attr('id', 'focusLineY')
+                        .attr('class', 'focusLine');
+                    */
+
+                    focus.append('circle')
+                        .attr('id', 'focusCircle')
+                        .attr('r', 1)
+                        .attr('class', 'circle focusCircle');
+
+                    /*
+                    focus.append("text")
+                        .attr('id', 'focusTexty')
+                        .attr("x", 9)
+                        .attr("dy", ".1em");
+                    focus.append("text")
+                        .attr('id', 'focusTextx')
+                        .attr("x", 40)
+                        .attr("dy", ".1em");
+                    */
+
+                    var bisectDate = d3.bisector(function(d) {
+                        return iso.parse(d.Start_ts);
+                    }).left;
+
+                    g.append('rect')
+                        .attr('class', 'overlay')
+                        .attr('width', width)
+                        .attr('height', height)
+                        .on('mouseover', function() {
+                            focus.style('display', null);
+                        })
+                    .on('mouseout', function() {
+                        focus.style('display', 'none');
+                    })
+                    .on('mousemove', function() {
+                        var mouse = d3.mouse(this);
+                        var mouseDate = xScale.invert(mouse[0]);
+                        var i = bisectDate(scope.data, mouseDate) || 1; // returns the index to the current data item
+                        var d0 = scope.data[i-1]
+                        var d1 = scope.data[i];
+                        // work out which date value is closest to the mouse
+                        var d = mouseDate - iso.parse(d0.Start_ts) > iso.parse(d1.Start_ts) - mouseDate ? d1 : d0;
+
+                        var x = xScale(iso.parse(d.Start_ts));
+                        var y = yScale(d.Query_count);
+
+                        var MIN = 0,
+                        MAX = 1;
+                        focus.select('#focusCircle')
+                            .attr('cx', x)
+                            .attr('cy', y);
+                        focus.select('#focusLineX')
+                            .attr('x1', x).attr('y1', yScale(yDomain[MIN]))
+                            .attr('x2', x).attr('y2', yScale(yDomain[MAX]));
+                        /*
+                        focus.select('#focusLineY')
+                            .attr('x1', xScale(xDomain[MIN])).attr('y1', y)
+                            .attr('x2', xScale(xDomain[MAX])).attr('y2', y);
+                        focus.select("#focusTexty")
+                            .attr("transform", "translate(" + xScale(xDomain[MIN]) + "," + (y + 3) + ")")
+                            // you may want to format the value here
+                            .text(d.Query_count);
+                        x = x - focus.select("#focusTextx").node().getComputedTextLength() * xScale2(iso.parse(d.Start_ts));
+                        focus.select("#focusTextx")
+                            .attr("transform", "translate(" + x + "," + (yScale(yDomain[MAX]) + 3) + ")")
+                            // you may want to format the value here
+                            .text(iso.parse(d.Start_ts));
+                            */
+                    });
+
+            }
+        };
+    });
+
+
 
 })();
