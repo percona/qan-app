41d013f4c26c9e35f170eccf39215a8eab657c1c
Andrii Skomorokhov <andrii.skomorokhov@percona.com>
2017-01-25 18:14:06 +0200

    Moving to angular cli.

--- app/core/page-not-found.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  template: `
-  <h1>Page Not Found</h1>
-  `
-})
-export class PageNotFoundComponent { }
--- app/shared/moment-format.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import * as moment from 'moment';
-
-@Pipe({ name: 'momentFormat' })
-export class MomentFormatPipe implements PipeTransform {
-  transform(value: moment.Moment, format: string): string {
-    return value.format(format);
-  }
-}
