1f0619024a2b80e08c947818034252f1581b172c
Andrii Skomorokhov <andrii.skomorokhov@percona.com>
2017-07-07 13:42:57 +0300

    [PMM-547] Added info alert how to configure pmm-client when instances list is empty.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+
+import { InstanceService } from './core/instance.service';
 import { environment } from './environment';
 
 @Component({
@@ -11,6 +13,12 @@ export class AppComponent {
   version = environment.version;
   isDemo = environment.demo;
   showNav = !this.inIframe();
+  isInstancesListEmpty: boolean;
+
+  constructor(instanceService: InstanceService) {
+    // show message how to configure pmm-client.
+    this.isInstancesListEmpty = !instanceService.dbServers.length;
+  }
 
   /**
    * inIframe is used to show/hide navbar.
