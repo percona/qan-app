5ddefede00c94d99edda8286347cd66678c43d32
Andrii Skomorokhov <andrii.skomorokhov@percona.com>
2018-05-31 18:09:52 +0300

    [PMM-2580] Added dist folder. Fixed lint.

--- src/app/mysql-query-details/mysql-query-details.component.ts
+++ src/app/mysql-query-details/mysql-query-details.component.ts
@@ -107,7 +107,11 @@ export class MySQLQueryDetailsComponent extends CoreComponent implements OnInit 
     // https://github.com/percona/go-mysql/blob/master/event/class.go#L25
     const maxExampleBytes = 10240;
     if (query.length >= maxExampleBytes) {
-      this.classicExplainError = this.jsonExplainError = `Cannot explain truncated query. This query was truncated to maximum size of ${maxExampleBytes} bytes.`;
+      this.jsonExplainError = `
+        Cannot explain truncated query.
+        This query was truncated to maximum size of ${maxExampleBytes} bytes.
+      `;
+      this.classicExplainError = this.jsonExplainError
       this.isExplainLoading = false;
       return
     }
