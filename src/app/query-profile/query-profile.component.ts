f5a60e2f7f5bfa53834ffd9a0bf3119d263d0178
Daria Lymanska <daria.lymanska@gmail.com>
2018-08-30 14:31:23 +0300

    Add checks for null search

--- src/app/query-profile/query-profile.component.ts
+++ src/app/query-profile/query-profile.component.ts
@@ -63,8 +63,8 @@ export class QueryProfileComponent extends CoreComponent {
         this.noQueryError = '';
         this.totalAmountOfQueries = this.leftInDbQueries = 0;
         this.queryProfile = [];
-
-        const search = this.queryParams.search === 'null' ? '' : this.queryParams.search;
+        this.searchValue = this.queryParams.search === 'null' ? '' : this.queryParams.search;
+        const search = this.queryParams.search === 'null' && this.searchValue !== 'NULL' ? '' : this.queryParams.search;
         const firstSeen = this.queryParams.first_seen;
         this.offset = 0;
         try {
@@ -90,7 +90,7 @@ export class QueryProfileComponent extends CoreComponent {
         this.isLoading = true;
         const dbServerUUID = this.dbServer.UUID;
         this.offset = this.offset + 10;
-        const search = this.queryParams.search === 'null' ? '' : this.queryParams.search;
+        const search = this.queryParams.search === 'null' && this.searchValue !== 'NULL' && this.searchValue !== 'null' ? '' : this.queryParams.search;
         const firstSeen = this.queryParams.first_seen;
         const data = await this.queryProfileService
             .getQueryProfile(dbServerUUID, this.fromUTCDate, this.toUTCDate, this.offset, search, firstSeen);
@@ -109,10 +109,10 @@ export class QueryProfileComponent extends CoreComponent {
         return queryParams;
     }
 
-    public async search() {
+    search() {
         const params: QueryParams = Object.assign({}, this.queryParams);
         if (!!this.searchValue) {
-            params.search = this.searchValue;
+            params.search = this.searchValue === 'null' ? 'NULL' : this.searchValue;
         } else {
             delete params.search;
         }
