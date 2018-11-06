73bccc5e40ebd39aed5c213e4dd0b9d7d3cc59b2
Daria Lymanska <daria.lymanska@gmail.com>
2018-11-06 14:05:25 +0200

    PMM-2816 Remove deprecated http methods in settings service

--- src/app/settings/settings.service.ts
+++ src/app/settings/settings.service.ts
@@ -1,6 +1,5 @@
-
-import { Injectable } from '@angular/core';
-import { Http, Headers, URLSearchParams } from '@angular/http';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 
 import 'rxjs/add/operator/toPromise';
 
@@ -9,79 +8,78 @@ export type CollectFrom = 'slowlog' | 'perfschema';
 @Injectable()
 export class SettingsService {
 
-    private headers = new Headers({ 'Content-Type': 'application/json' });
+  private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
 
-    constructor(private http: Http) { }
+  constructor(private httpClient: HttpClient) {
+  }
 
-    public async getAgentStatus(agentUUID: string): Promise<{}> {
-        const url = `/qan-api/agents/${agentUUID}/status`;
+  public async getAgentStatus(agentUUID: string): Promise<{}> {
+    const url = `/qan-api/agents/${agentUUID}/status`;
 
-        const response = await this.http
-            .get(url, { headers: this.headers })
-            .toPromise();
-        return response.json() as {};
+    const response = await this.httpClient
+      .get(url, {headers: this.httpHeaders})
+      .toPromise();
+    return response as {};
+  }
+
+  public async getAgentLog(agentUUID, begin, end: string): Promise<{}> {
+    const url = `/qan-api/agents/${agentUUID}/log`;
+
+    let httpParams = new HttpParams();
+    httpParams = httpParams.append('begin', begin);
+    httpParams = httpParams.append('end', end);
+    const response = await this.httpClient
+      .get(url, {headers: this.httpHeaders, params: httpParams})
+      .toPromise();
+    return response as {};
+  }
+
+  public async getAgentDefaults(agentUUID: string, dbServerUUID: string): Promise<{}> {
+    const url = `/qan-api/agents/${agentUUID}/cmd`;
+    const params = {
+      AgentUUID: agentUUID,
+      Service: 'agent',
+      Cmd: 'GetDefaults',
+      Data: btoa(JSON.stringify({UUID: dbServerUUID}))
+    };
+
+    const resp: any = await this.httpClient
+      .put(url, params, {headers: this.httpHeaders})
+      .toPromise();
+
+    if (!!resp.Error) {
+      throw new Error(resp.Error);
+    } else {
+      return JSON.parse(atob(resp.Data));
     }
+  }
 
-    public async getAgentLog(agentUUID, begin, end: string): Promise<{}> {
-        const url = `/qan-api/agents/${agentUUID}/log`;
+  public async setAgentDefaults(agentUUID: string, dbServerUUID: string, interval: number,
+                                exampleQueries: boolean, collectFrom: CollectFrom): Promise<{}> {
+    const url = `/qan-api/agents/${agentUUID}/cmd`;
 
-        const params = new URLSearchParams();
-        params.set('begin', begin);
-        params.set('end', end);
-        const response = await this.http
-            .get(url, { headers: this.headers, search: params })
-            .toPromise();
-        return response.json() as {};
+    const data = {
+      UUID: dbServerUUID,
+      Interval: interval * 60,
+      ExampleQueries: exampleQueries,
+      CollectFrom: collectFrom
+    };
+
+    const params = {
+      AgentUUID: agentUUID,
+      Service: 'qan',
+      Cmd: 'RestartTool',
+      Data: btoa(JSON.stringify(data))
+    };
+
+    const resp: any = await this.httpClient
+      .put(url, params, {headers: this.httpHeaders})
+      .toPromise();
+
+    if (!!resp.Error) {
+      throw new Error(resp.Error);
+    } else {
+      return JSON.parse(atob(resp.Data));
     }
-
-    public async getAgentDefaults(agentUUID: string, dbServerUUID: string): Promise<{}> {
-        const url = `/qan-api/agents/${agentUUID}/cmd`;
-        const params = {
-            AgentUUID: agentUUID,
-            Service: 'agent',
-            Cmd: 'GetDefaults',
-            Data: btoa(JSON.stringify({ UUID: dbServerUUID }))
-        };
-
-        const response = await this.http
-            .put(url, params, { headers: this.headers })
-            .toPromise();
-
-        const resp = response.json();
-        if (!!resp.Error) {
-            throw new Error(resp.Error);
-        } else {
-            return JSON.parse(atob(resp.Data));
-        }
-    }
-
-    public async setAgentDefaults(agentUUID: string, dbServerUUID: string, interval: number,
-        exampleQueries: boolean, collectFrom: CollectFrom): Promise<{}> {
-        const url = `/qan-api/agents/${agentUUID}/cmd`;
-
-        const data = {
-            UUID: dbServerUUID,
-            Interval: interval * 60,
-            ExampleQueries: exampleQueries,
-            CollectFrom: collectFrom
-        };
-
-        const params = {
-            AgentUUID: agentUUID,
-            Service: 'qan',
-            Cmd: 'RestartTool',
-            Data: btoa(JSON.stringify(data))
-        };
-
-        const response = await this.http
-            .put(url, params, { headers: this.headers })
-            .toPromise();
-
-        const resp = response.json();
-        if (!!resp.Error) {
-            throw new Error(resp.Error);
-        } else {
-            return JSON.parse(atob(resp.Data));
-        }
-    }
+  }
 }
