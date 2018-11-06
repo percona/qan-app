a119170ce4c9edc3534139c9dfd83f105ab0e792
Daria Lymanska <daria.lymanska@gmail.com>
2018-11-06 13:40:22 +0200

    PMM-2816 Remove deprecated http methods for query profile service

--- src/app/mysql-query-details/mysql-query-details.service.ts
+++ src/app/mysql-query-details/mysql-query-details.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/toPromise';
-import {HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class MySQLQueryDetailsService {
