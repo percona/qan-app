1ea5a9b1e1911c72d7eda794444dec14284605af
Daria Lymanska <daria.lymanska@gmail.com>
2018-11-23 10:32:07 +0200

    PMM-3211 Add disable main checkbox when sub main properties are not checked

--- src/app/qan-edit-column/qan-edit-column.component.ts
+++ src/app/qan-edit-column/qan-edit-column.component.ts
@@ -11,27 +11,38 @@ export class QanEditColumnComponent implements OnDestroy {
   public isConfigurationMenu = false;
   public columns: any;
   private subscription: any;
+  public isMainChecked: any;
+  public isSubMain: any;
 
   constructor(public configService: QueryTableConfigurationService) {
-    this.subscription = this.configService.source.subscribe(item => {
-      this.columns = item;
+    this.subscription = this.configService.source.subscribe(items => {
+      this.columns = items;
     });
   }
 
-  // isMainChecked() {
-  //   const isLoad = this.columns[0].sparkline || this.columns[0].value || this.columns[0].percentage;
-  //   const isCount = this.columns[1].sparkline || this.columns[1].queriesPerSecond || this.columns[1].value || this.columns[1].percentage;
-  //   const isLatency = this.columns[2].sparkline || this.columns[2].value || this.columns[2].distribution;
-  //   if (!isLoad) {
-  //     this.configService.toggleConfig('load', 'checked')
-  //   }
-  //   if (!isCount) {
-  //     this.configService.toggleConfig('count', 'checked')
-  //   }
-  //   if (!isLatency) {
-  //     this.configService.toggleConfig('latency', 'checked')
-  //   }
-  // }
+  checkMain(id, currentKey) {
+    this.configService.toggleConfig(id, currentKey);
+
+    switch (id) {
+      case 'load':
+        this.isSubMain = this.columns[0].sparkline || this.columns[0].value || this.columns[0].percentage;
+        this.isMainChecked = this.columns[0].checked && this.isSubMain;
+        break;
+      case 'count':
+        this.isSubMain =
+          this.columns[1].sparkline || this.columns[1].value || this.columns[1].percentage || this.columns[1].queriesPerSecond;
+        this.isMainChecked = this.columns[1].checked && this.isSubMain;
+        break;
+      case 'latency':
+        this.isSubMain = this.columns[2].sparkline || this.columns[2].value || this.columns[2].distribution;
+        this.isMainChecked = this.columns[2].checked && this.isSubMain;
+        break;
+    }
+
+    if (!this.isMainChecked) {
+      this.configService.toggleConfig(id, 'checked');
+    }
+  }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
