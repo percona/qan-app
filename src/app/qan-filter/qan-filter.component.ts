d8daeb52e464d64bdddb08e45f791ec565be4904
Daria Lymanska <daria.lymanska@gmail.com>
2018-12-20 15:33:43 +0200

    PMM-3318 Add toggle functional implemetation for filter list parameters

--- src/app/qan-filter/qan-filter.component.ts
+++ src/app/qan-filter/qan-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {BehaviorSubject} from 'rxjs/Rx';
 
 @Component({
@@ -9,30 +9,43 @@ import {BehaviorSubject} from 'rxjs/Rx';
 export class QanFilterComponent implements OnInit {
 
   public isToggleMenu = false;
-  public limitParameters = 5;
+  public limitParameters = {};
+  public defaultLimit = 4;
+  public filterMenuCategories: Array<{}> = [];
+  public seeAllTitle = 'see all';
 
-  public filterMenuCategories: Array<{}> = [
-    {
-      name: 'Schemas',
-      parameters: ['performance_schema', 'wordpress', 'my_DB1', 'my_DB2', 'innodb_2']
-    },
-    {
-      name: 'Servers',
-      parameters: ['mdb101', 'mdb102', 'ps55', 'ps57myrocks', 'pxc56-2']
-    },
-    {
-      name: 'Users',
-      parameters: ['wp_user', 'pmm', 'sysbench']
-    },
-    {
-      name: 'Hosts',
-      parameters: ['127.0.0.1', '192.168.1.1', '172.56.33.5']
-    },
-  ];
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.filterMenuCategories = [
+      {
+        name: 'Schemas',
+        parameters:
+          ['performance_schema', 'wordpress', 'my_DB1', 'my_DB2', 'innodb_2', 'performance_schema-1', 'wordpress-1', 'my_DB1-1', 'my_DB2-1',
+            'innodb_2-1']
+      },
+      {
+        name: 'Servers',
+        parameters: ['mdb101', 'mdb102', 'ps55', 'ps57myrocks', 'pxc56-2', 'mdb101-1', 'mdb102-1', 'ps55-1', 'ps57myrocks-1', 'pxc56-2-1']
+      },
+      {
+        name: 'Users',
+        parameters: ['wp_user', 'pmm', 'sysbench']
+      },
+      {
+        name: 'Hosts',
+        parameters: ['127.0.0.1', '192.168.1.1', '172.56.33.5']
+      },
+    ];
+
+    this.filterMenuCategories.forEach(item => {
+      this.limitParameters[item['name']] = this.defaultLimit;
+    });
+  }
+
+  getAll(currentCategory) {
+    this.limitParameters[currentCategory.name] =
+      this.limitParameters[currentCategory.name] <= 4 ? currentCategory.parameters.length - 1 : this.defaultLimit
+  }
 }
