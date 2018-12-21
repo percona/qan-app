be74412ce8129daca3a6a555cc54146c4a9e687a
Daria Lymanska <daria.lymanska@gmail.com>
2018-12-21 18:50:53 +0200

    PMM-3318 Refactor qan-configuration service

--- src/app/query-profile/query-table-configuration.service.ts
+++ src/app/query-profile/query-table-configuration.service.ts
@@ -8,27 +8,27 @@ export class QueryTableConfigurationService {
     {
       name: 'Load',
       id: 'load',
-      checked: localStorage.getItem('load-checked') === null ? true : !!localStorage.getItem('load-checked'),
-      sparkline: localStorage.getItem('load-sparkline') === null ? true : !!localStorage.getItem('load-sparkline'),
-      value: localStorage.getItem('load-value') === null ? true : !!localStorage.getItem('load-value'),
-      percentage: localStorage.getItem('load-percentage') === null ? true : !!localStorage.getItem('load-percentage')
+      checked: localStorage.getItem('load-checked') === null || !!localStorage.getItem('load-checked'),
+      sparkline: localStorage.getItem('load-sparkline') === null || !!localStorage.getItem('load-sparkline'),
+      value: localStorage.getItem('load-value') === null || !!localStorage.getItem('load-value'),
+      percentage: localStorage.getItem('load-percentage') === null || !!localStorage.getItem('load-percentage')
     },
     {
       name: 'Count',
       id: 'count',
-      checked: localStorage.getItem('count-checked') === null ? true : !!localStorage.getItem('count-checked'),
-      queriesPerSecond: localStorage.getItem('count-queriesPerSecond') === null ? true : !!localStorage.getItem('count-queriesPerSecond'),
-      sparkline: localStorage.getItem('count-sparkline') === null ? true : !!localStorage.getItem('count-sparkline'),
-      value: localStorage.getItem('count-value') === null ? true : !!localStorage.getItem('count-value'),
-      percentage: localStorage.getItem('count-percentage') === null ? true : !!localStorage.getItem('count-percentage')
+      checked: localStorage.getItem('count-checked') === null || !!localStorage.getItem('count-checked'),
+      queriesPerSecond: localStorage.getItem('count-queriesPerSecond') === null || !!localStorage.getItem('count-queriesPerSecond'),
+      sparkline: localStorage.getItem('count-sparkline') === null || !!localStorage.getItem('count-sparkline'),
+      value: localStorage.getItem('count-value') === null || !!localStorage.getItem('count-value'),
+      percentage: localStorage.getItem('count-percentage') === null || !!localStorage.getItem('count-percentage')
     },
     {
       name: 'Avg Latency',
       id: 'latency',
-      checked: localStorage.getItem('latency-checked') === null ? true : !!localStorage.getItem('latency-checked'),
-      sparkline: localStorage.getItem('latency-sparkline') === null ? true : !!localStorage.getItem('latency-sparkline'),
-      value: localStorage.getItem('latency-value') === null ? true : !!localStorage.getItem('latency-value'),
-      distribution: localStorage.getItem('latency-distribution') === null ? true : !!localStorage.getItem('latency-distribution')
+      checked: localStorage.getItem('latency-checked') === null || !!localStorage.getItem('latency-checked'),
+      sparkline: localStorage.getItem('latency-sparkline') === null || !!localStorage.getItem('latency-sparkline'),
+      value: localStorage.getItem('latency-value') === null || !!localStorage.getItem('latency-value'),
+      distribution: localStorage.getItem('latency-distribution') === null || !!localStorage.getItem('latency-distribution')
     }
   ]);
 
