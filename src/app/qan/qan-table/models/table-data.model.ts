531a1b6e2bf10cd793446594e6da1792a643e5f0
Daria Lymanska <daria.lymanska@gmail.com>
2019-04-01 09:40:09 +0300

    PMM-3744 Add fingerprint and update fields

--- src/app/qan/qan-table/models/table-data.model.ts
+++ src/app/qan/qan-table/models/table-data.model.ts
@@ -5,13 +5,21 @@ export class TableDataModel {
   metrics: Array<MetricModel>;
   sparkline: Array<Object>;
   dimension: string;
+  fingerprint: string;
   rank: number;
+  load: number;
+  num_queries: number;
+  qps: number;
 
   constructor(row) {
     this.metrics = Object.entries(row.metrics).map(item => new MetricModel(item));
     this.sparkline = row.sparkline.map(sparklineValue => sparklineValue.values);
     this.sparkline.forEach(item => item['timestamp'] = moment.unix(item['timestamp']).format('YYYY-MM-DDTHH:mm:ssZ'));
     this.dimension = row.dimension || '';
+    this.fingerprint = row.fingerprint || '';
     this.rank = row.rank || 0;
+    this.load = row.load || '';
+    this.num_queries = row.num_queries || '';
+    this.qps = row.qps || '';
   }
 }
