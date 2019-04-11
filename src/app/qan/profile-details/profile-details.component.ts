86c058c603c3cab0f0cfa06bb5fb22670e9fb42a
Daria Lymanska <daria.lymanska@gmail.com>
2019-04-11 17:48:00 +0300

    PMM-3574 Add base styles for query details

--- src/app/qan/profile-details/profile-details.component.ts
+++ src/app/qan/profile-details/profile-details.component.ts
@@ -19,9 +19,7 @@ export class ProfileDetailsComponent implements OnInit, AfterViewChecked {
   public currentParams: any;
   public dimension: string;
   public details: MetricModel[] = [];
-  public yKey: string;
-  public measurement: string;
-  public pipeType: string;
+
   event = new Event('showSuccessNotification');
 
   constructor(
@@ -49,54 +47,6 @@ export class ProfileDetailsComponent implements OnInit, AfterViewChecked {
   }
 
   ngOnInit() {
-  }
-
-  setCurrentSparkline(name: string) {
-    switch (name) {
-      case 'load':
-        this.yKey = 'm_query_load';
-        this.measurement = 'number';
-        this.pipeType = 'number';
-        break;
-      case 'count':
-        this.yKey = 'num_queries_sum';
-        this.measurement = 'number';
-        this.pipeType = 'number';
-        break;
-      case 'latency':
-        this.yKey = 'm_query_time_avg';
-        this.measurement = 'time';
-        break;
-      case 'bytes_sent':
-        this.yKey = 'm_bytes_sent_sum';
-        this.measurement = 'size';
-        this.pipeType = 'size';
-        break;
-      case 'lock_time':
-        this.yKey = 'm_lock_time_sum';
-        this.measurement = 'number';
-        this.pipeType = 'time';
-        break;
-      case 'query_time':
-        this.yKey = 'm_query_time_avg';
-        this.measurement = 'time';
-        this.pipeType = 'time';
-        break;
-      case 'rows_sent':
-        this.yKey = 'm_rows_sent_sum';
-        this.measurement = 'number';
-        this.pipeType = 'number';
-        break;
-      case 'rows_examined':
-        this.yKey = 'm_rows_examined_sum';
-        this.measurement = 'number';
-        break;
-      default: {
-        this.yKey = '';
-        this.measurement = '';
-        break;
-      }
-    }
   }
 
   ngAfterViewChecked() {
