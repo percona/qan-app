9896d8cf7f38e6a5a2d6cce239f0853b2a38ced4
Daria Lymanska <daria.lymanska@gmail.com>
2019-04-26 15:20:29 +0300

    PMM-3915 Rewrite logic for checking datatype for details

--- src/app/qan/profile-details/components/details-row/details-row.component.ts
+++ src/app/qan/profile-details/components/details-row/details-row.component.ts
@@ -13,136 +13,64 @@ export class DetailsRowComponent implements OnInit {
   public measurement: string;
   public pipeType: string;
   public additionalPipeType: string;
+  public ratePipe: string;
+  public sumPipe: string;
+  public subSumPipe: string;
+  public perQueryStatsPipe: string;
 
   constructor() {
   }
 
   ngOnInit() {
-    this.setCurrentSparkline(this.currentMetric.metricName);
+    this.setDataFormat(this.currentMetric.metricName);
   }
 
-  setCurrentSparkline(name: string) {
+  setDataFormat(name: string) {
     switch (name) {
-      case 'load':
-        this.yKey = 'm_query_load';
-        this.measurement = 'number';
-        this.pipeType = 'number';
+      case 'bytes_sent' || 'innodb_io_r_bytes' || 'tmp_table_sizes':
+        this.ratePipe = 'size';
+        this.sumPipe = 'size';
+        this.subSumPipe = 'percent';
+        this.perQueryStatsPipe = 'size';
         break;
-      case 'count':
-        this.yKey = 'num_queries_sum';
-        this.measurement = 'number';
-        this.pipeType = 'number';
+      case 'lock_time' || 'query_time' || 'innodb_io_r_wait' || 'innodb_queue_wait' || 'innodb_rec_lock_wait':
+        this.ratePipe = 'number';
+        this.sumPipe = 'time';
+        this.subSumPipe = 'percent';
+        this.perQueryStatsPipe = 'time';
         break;
-      case 'latency':
-        this.yKey = 'm_query_time_avg';
-        this.measurement = 'time';
-        this.pipeType = 'time';
-        break;
-      case 'bytes_sent':
-        this.yKey = 'm_bytes_sent_sum';
-        this.measurement = 'size';
-        this.pipeType = 'size';
-        break;
-      case 'lock_time':
-        this.yKey = 'm_lock_time_sum';
-        this.measurement = 'number';
-        this.pipeType = 'time';
-        break;
-      case 'query_time':
-        this.yKey = 'm_query_time_avg';
-        this.measurement = 'time';
-        this.pipeType = 'time';
-        break;
-      case 'rows_sent':
-        this.yKey = 'm_rows_sent_sum';
-        this.measurement = 'number';
-        this.pipeType = 'number';
-        break;
-      case 'rows_examined':
-        this.yKey = 'm_rows_examined_sum';
-        this.measurement = 'number';
-        break;
-      case 'filesort' || 'filesort_on_disk' || 'full_join' || 'full_scan' || 'innodb_io_r_ops' || 'response_length':
-        this.yKey = 'm_rows_examined_sum';
-        this.measurement = 'number';
-        this.pipeType = 'number';
-        break;
-      case 'innodb_io_r_bytes':
-        this.yKey = 'm_rows_examined_sum';
-        this.measurement = 'size';
-        this.pipeType = 'size';
-        break;
-      case 'innodb_io_r_wait':
-        this.yKey = 'm_rows_examined_sum';
-        this.measurement = 'size';
-        this.pipeType = 'size';
-        this.additionalPipeType = 'time';
+      case 'filesort' || 'filesort_on_disk' || 'full_scan' || 'full_join':
+        this.ratePipe = 'number';
+        this.sumPipe = 'number';
+        this.subSumPipe = 'percent';
+        this.perQueryStatsPipe = 'size'; // absent
         break;
       case 'innodb_pages_distinct':
-        this.yKey = 'm_rows_examined_sum';
-        this.measurement = 'number';
-        this.pipeType = 'number';
+        this.ratePipe = ''; // absent
+        this.sumPipe = '';  // absent
+        this.subSumPipe = ''; // absent
+        this.perQueryStatsPipe = 'number';
         break;
-      case 'innodb_queue_wait':
-        this.yKey = 'm_rows_examined_sum';
-        this.measurement = 'number';
-        this.pipeType = 'number';
-        this.additionalPipeType = 'time';
+      case 'rows_examined' || 'rows_sent' || 'rows_affected' || 'merge_passes' || 'select_range_check' ||
+        'tmp_disk_tables' || 'innodb_io_r_ops' || 'innodb_io_r_ops' || 'no_good_index_used' ||
+        'query_length' || 'no_index_used' || 'rows_read' || 'select_full_range_join' || 'select_range' ||
+        'sort_range' || 'sort_rows' || 'sort_scan' || 'response_length':
+        this.ratePipe = 'number';
+        this.sumPipe = 'number';
+        this.subSumPipe = 'percent';
+        this.perQueryStatsPipe = 'number';
         break;
-      case 'innodb_rec_lock_wait':
-        this.yKey = 'm_rows_examined_sum';
-        this.measurement = 'time';
-        this.pipeType = 'time';
-        this.additionalPipeType = '';
-        break;
-      case 'merge_passes':
-        this.yKey = 'm_rows_examined_sum';
-        this.measurement = 'number';
-        this.pipeType = 'number';
-        this.additionalPipeType = '';
-        break;
-      case 'qc_hit':
-        this.yKey = 'm_rows_examined_sum';
-        this.measurement = 'number';
-        this.pipeType = 'number';
-        this.additionalPipeType = '';
-        break;
-      case 'tmp_disk_tables':
-        this.yKey = 'm_rows_examined_sum';
-        this.measurement = 'number';
-        this.pipeType = 'number';
-        this.additionalPipeType = '';
-        break;
-      case 'tmp_table_on_disk':
-        this.yKey = 'm_rows_examined_sum';
-        this.measurement = 'number';
-        this.pipeType = 'number';
-        this.additionalPipeType = '';
-        break;
-      case 'tmp_table_sizes':
-        this.yKey = 'm_rows_examined_sum';
-        this.measurement = 'size';
-        this.pipeType = 'size';
-        this.additionalPipeType = '';
-        break;
-      case 'tmp_table' || 'tmp_table':
-        this.yKey = 'm_rows_examined_sum';
-        this.measurement = 'number';
-        this.pipeType = 'number';
-        this.additionalPipeType = '';
-        break;
-      case
-        'no_good_index_used' || 'query_length' || 'no_index_used' || 'rows_affected' || 'rows_read' || 'select_full_range_join'
-        || 'select_range' || 'select_range_check' || 'sort_range' || 'sort_rows' || 'sort_scan':
-        this.yKey = '????';
-        this.measurement = '???';
-        this.pipeType = '???';
-        this.additionalPipeType = '???';
+      case 'qc_hit' || 'tmp_table_on_disk' || 'tmp_table':
+        this.ratePipe = 'number';
+        this.sumPipe = 'number';
+        this.subSumPipe = 'percent';
+        this.perQueryStatsPipe = ''; // absent
         break;
       default: {
         this.yKey = '';
         this.measurement = '';
         this.pipeType = '';
+        this.subSumPipe = '';
         this.additionalPipeType = '';
         break;
       }
