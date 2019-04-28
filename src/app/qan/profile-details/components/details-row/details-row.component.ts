5fa89612a13ea5da2b8a6ce46900f952c2fd5b0a
Daria Lymanska <daria.lymanska@gmail.com>
2019-04-28 22:31:24 +0300

    PMM-3915 Rewrite checking logic

--- src/app/qan/profile-details/components/details-row/details-row.component.ts
+++ src/app/qan/profile-details/components/details-row/details-row.component.ts
@@ -9,6 +9,77 @@ import { MetricModel } from '../../../profile-table/models/metric.model';
 export class DetailsRowComponent implements OnInit {
   @Input() currentMetric: MetricModel;
 
+  private numberTypes = [
+    'rows_sent', 'rows_examined', 'rows_affected', 'merge_passes', 'select_range_check', 'tmp_disk_tables',
+    'innodb_io_r_ops', 'no_good_index_used', 'query_length', 'no_index_used', 'rows_read', 'tmp_tables',
+    'select_full_range_join', 'select_range', 'sort_range', 'sort_rows', 'sort_scan', 'response_length',
+    'docs_returned', 'docs_scanned'
+  ];
+
+  private sizeTypes = [
+    'bytes_sent', 'innodb_io_r_bytes', 'tmp_table_sizes'
+  ];
+
+  private timeTypes = [
+    'lock_time', 'query_time', 'innodb_io_r_wait', 'innodb_queue_wait', 'innodb_rec_lock_wait'
+  ];
+
+  private fileSortTypes = [
+    'filesort', 'filesort_on_disk', 'full_scan', 'full_join', 'qc_hit', 'tmp_table_on_disk', 'tmp_table'
+  ];
+
+  private pagesDistinctTypes = [
+    'innodb_pages_distinct'
+  ];
+
+  public conditions = [
+    {
+      isIncluded: name => this.numberTypes.includes(name),
+      pipeTypes: {
+        ratePipe: 'number',
+        sumPipe: 'number',
+        subSumPipe: 'percent',
+        perQueryStatsPipe: 'number'
+      }
+    },
+    {
+      isIncluded: name => this.sizeTypes.includes(name),
+      pipeTypes: {
+        ratePipe: 'size',
+        sumPipe: 'size',
+        subSumPipe: 'percent',
+        perQueryStatsPipe: 'size'
+      }
+    },
+    {
+      isIncluded: name => this.timeTypes.includes(name),
+      pipeTypes: {
+        ratePipe: 'number',
+        sumPipe: 'time',
+        subSumPipe: 'percent',
+        perQueryStatsPipe: 'time'
+      }
+    },
+    {
+      isIncluded: name => this.fileSortTypes.includes(name),
+      pipeTypes: {
+        ratePipe: 'number',
+        sumPipe: 'number',
+        subSumPipe: 'percent',
+        perQueryStatsPipe: ''
+      }
+    },
+    {
+      isIncluded: name => this.pagesDistinctTypes.includes(name),
+      pipeTypes: {
+        ratePipe: '',
+        sumPipe: '',
+        subSumPipe: '',
+        perQueryStatsPipe: 'number'
+      }
+    }
+  ];
+
   public yKey: string;
   public measurement: string;
   public pipeType: string;
@@ -26,54 +97,12 @@ export class DetailsRowComponent implements OnInit {
   }
 
   setDataFormat(name: string) {
-    switch (name) {
-      case 'bytes_sent' || 'innodb_io_r_bytes' || 'tmp_table_sizes':
-        this.ratePipe = 'size';
-        this.sumPipe = 'size';
-        this.subSumPipe = 'percent';
-        this.perQueryStatsPipe = 'size';
-        break;
-      case 'lock_time' || 'query_time' || 'innodb_io_r_wait' || 'innodb_queue_wait' || 'innodb_rec_lock_wait':
-        this.ratePipe = 'number';
-        this.sumPipe = 'time';
-        this.subSumPipe = 'percent';
-        this.perQueryStatsPipe = 'time';
-        break;
-      case 'filesort' || 'filesort_on_disk' || 'full_scan' || 'full_join':
-        this.ratePipe = 'number';
-        this.sumPipe = 'number';
-        this.subSumPipe = 'percent';
-        this.perQueryStatsPipe = 'size'; // absent
-        break;
-      case 'innodb_pages_distinct':
-        this.ratePipe = ''; // absent
-        this.sumPipe = '';  // absent
-        this.subSumPipe = ''; // absent
-        this.perQueryStatsPipe = 'number';
-        break;
-      case 'rows_examined' || 'rows_sent' || 'rows_affected' || 'merge_passes' || 'select_range_check' ||
-        'tmp_disk_tables' || 'innodb_io_r_ops' || 'innodb_io_r_ops' || 'no_good_index_used' ||
-        'query_length' || 'no_index_used' || 'rows_read' || 'select_full_range_join' || 'select_range' ||
-        'sort_range' || 'sort_rows' || 'sort_scan' || 'response_length':
-        this.ratePipe = 'number';
-        this.sumPipe = 'number';
-        this.subSumPipe = 'percent';
-        this.perQueryStatsPipe = 'number';
-        break;
-      case 'qc_hit' || 'tmp_table_on_disk' || 'tmp_table':
-        this.ratePipe = 'number';
-        this.sumPipe = 'number';
-        this.subSumPipe = 'percent';
-        this.perQueryStatsPipe = ''; // absent
-        break;
-      default: {
-        this.yKey = '';
-        this.measurement = '';
-        this.pipeType = '';
-        this.subSumPipe = '';
-        this.additionalPipeType = '';
-        break;
-      }
-    }
+    const filtered = this.conditions.filter(condition => condition.isIncluded(name))[0];
+    const { ratePipe = '', sumPipe = '', subSumPipe = '', perQueryStatsPipe = '' } = filtered.pipeTypes || {};
+
+    this.ratePipe = ratePipe;
+    this.sumPipe = sumPipe;
+    this.subSumPipe = subSumPipe;
+    this.perQueryStatsPipe = perQueryStatsPipe;
   }
 }
