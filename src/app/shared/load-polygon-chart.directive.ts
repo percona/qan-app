2a05cc3bb1081eb55d65205e4b3ec0c75fa5d9c6
Daria Lymanska <daria.lymanska@gmail.com>
2019-05-20 15:08:07 +0300

    PMM-4049 Add hover for sparkline (#222)
    * PMM-4049 Debug code
    
    * PMM-4049 Add circle output
    
    * PMM-4049 Add stroke for sparklines
    
    * PMM-4049 Add x axis line and tooltip data
    
    * PMM-4049 Add nodata output

--- src/app/shared/load-polygon-chart.directive.ts
+++ src/app/shared/load-polygon-chart.directive.ts
@@ -3,7 +3,11 @@ import { HumanizePipe } from './humanize.pipe';
 import { area, curveStepAfter } from 'd3-shape';
 import * as moment from 'moment';
 import { scaleLinear } from 'd3-scale';
-import { select } from 'd3-selection';
+import { event as currentEvent, mouse, select } from 'd3-selection';
+import { isoParse } from 'd3-time-format';
+import { bisector } from 'd3-array';
+import { MomentFormatPipe } from './moment-format.pipe';
+import { axisBottom } from 'd3-axis';
 
 export interface DataType {
   x: any,
@@ -16,8 +20,8 @@ export interface DataType {
 export class LoadPolygonChartDirective implements OnChanges {
   public margin = 0;
   public padding = 0;
-  public data = [];
-
+  public data: any = [];
+  dateFormat = new MomentFormatPipe();
   humanize = new HumanizePipe();
 
   @HostBinding('attr.data-tooltip')
@@ -91,5 +95,69 @@ export class LoadPolygonChartDirective implements OnChanges {
     g.append('path')
       .attr('d', areaBar(this.data))
       .style('fill', '#d9721f');
+
+    const focus = g.append('g').style('display', 'none');
+
+    focus.append('line')
+      .attr('id', 'focusLineX')
+      .attr('class', 'focusLine');
+
+    focus.append('circle')
+      .attr('id', 'focusCircle')
+      .attr('r', 1.5)
+      .attr('class', 'circle focusCircle');
+
+    focus.append('text')
+      .attr('id', 'focusText')
+      .attr('font-size', '10')
+      .attr('x', 1)
+      .attr('y', 8);
+
+    // @ts-ignore TS2345
+    const bisectDate = bisector((d, x) => +moment.utc(d[this.xkey]).isBefore(x)).right;
+
+    const rect = g.append('rect')
+      .attr('class', 'overlay')
+      .attr('width', this.width)
+      .attr('height', this.height)
+      .on('mouseover', () => focus.style('display', null))
+      .on('mouseout', () => focus.style('display', 'none'));
+
+    rect.on('mousemove', () => {
+      const coords = mouse(currentEvent.currentTarget);
+
+      const mouseDate: any = moment.utc(scaleX.invert(coords[0]));
+      // returns the index to the current data item
+      const i = Math.min(Math.max(bisectDate(this.appLoadPolygonChart, mouseDate), 0), this.appLoadPolygonChart.length - 1);
+      const d = this.appLoadPolygonChart[i];
+
+      const x = scaleX(isoParse(d[this.xkey]));
+      const y = scaleY(d[this.ykey] === undefined ? 0 : d[this.ykey]);
+
+      focus.select('#focusCircle')
+        .attr('cx', x)
+        .attr('cy', y);
+
+      focus.select('#focusLineX')
+        .attr('x1', x)
+        .attr('y1', scaleY(this.findMinY()))
+        .attr('x2', x)
+        .attr('y2', scaleY(this.findHighestY()));
+
+      const value = d[this.ykey] === undefined ? 0 : d[this.ykey];
+      const load = this.humanize.transform(value, this.measurement);
+
+      const dateToShow = this.dateFormat.transform(moment(d[this.xkey]).utc());
+      this.dataTooltip = !value ? `No data at ${dateToShow}` : `${load} at ${dateToShow}`;
+    });
+
+    // Create X axis
+    const xAxis = axisBottom(scaleX);
+
+    svg.append('g')
+      .attr('class', 'x-axis')
+      .attr('transform',
+        `translate(${this.margin},${this.height - this.margin - 1})`)
+      .call(xAxis);
   }
 }
