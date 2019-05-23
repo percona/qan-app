8fa7cae2b4f80fc9222a5f832ff5a77c5f97000f
Daria Lymanska <daria.lymanska@gmail.com>
2019-05-23 17:42:05 +0300

    PMM-4090 Add percent output for filters

--- src/app/qan/filter-menu/models/filter.model.ts
+++ src/app/qan/filter-menu/models/filter.model.ts
@@ -7,10 +7,16 @@ export class FilterModel {
     if (typeof (item) === 'object') {
       const { value, main_metric_percent } = item;
       this.value = value || 'unknown';
-      this.main_metric_percent = main_metric_percent || '';
+      this.main_metric_percent = this.percentFromNumber(1, main_metric_percent) || '';
     } else {
       this.value = item
     }
     this.state = false;
   }
+
+  percentFromNumber(total, current) {
+    return current
+    // return ((+current / +total) * 100).toFixed(2)
+  }
+
 }
