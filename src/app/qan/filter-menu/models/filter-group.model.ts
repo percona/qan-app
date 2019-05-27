65cd586cbd7055c78399a80c25e2808047ef7de9
Daria Lymanska <daria.lymanska@gmail.com>
2019-05-27 15:37:19 +0300

    PMM-4100 Add numan labels names

--- src/app/qan/filter-menu/models/filter-group.model.ts
+++ src/app/qan/filter-menu/models/filter-group.model.ts
@@ -12,7 +12,30 @@ export class FilterGroupModel {
     } else {
       itemsArray = itemsGroup.name.map(item => new FilterModel(item));
     }
-    this.filterGroup = filterGroup;
+    this.filterGroup = this.humanNamesForGroup(filterGroup);
     this.items = itemsArray;
+  }
+
+  humanNamesForGroup(groupName) {
+    switch (groupName) {
+      case 'environment':
+        return 'Environment';
+      case 'cluster':
+        return 'Cluster';
+      case 'replication_set':
+        return 'Replication Set';
+      case 'd_database':
+        return 'Database';
+      case 'd_schema':
+        return 'Schema';
+      case 'd_server':
+        return 'Server';
+      case 'd_client_host':
+        return 'Client Host';
+      case 'd_username':
+        return 'User Name';
+      default:
+        return groupName
+    }
   }
 }
