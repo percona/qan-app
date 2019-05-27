a3007ab8ee82fbc91d58923510d617a6d203a55d
Daria Lymanska <daria.lymanska@gmail.com>
2019-05-27 16:11:08 +0300

    PMM-4100 Add humanize for labels grouo names

--- src/app/qan/filter-menu/filter-menu.service.ts
+++ src/app/qan/filter-menu/filter-menu.service.ts
@@ -71,4 +71,27 @@ export class FilterMenuService {
       this.qanProfileService.updateObjectDetails(detailsUrlParams);
     }
   }
+
+  humanNamesForGroup(groupName) {
+    switch (groupName) {
+      case 'environment':
+        return 'Environment';
+      case 'cluster':
+        return 'Cluster';
+      case 'replication_set':
+        return 'Replication Set';
+      case 'd_database':
+        return 'Database';
+      case 'd_schema':
+        return 'Schema';
+      case 'd_server':
+        return 'Server';
+      case 'd_client_host':
+        return 'Client Host';
+      case 'd_username':
+        return 'User Name';
+      default:
+        return groupName
+    }
+  }
 }
--- src/app/qan/filter-menu/models/filter-group.model.ts
+++ src/app/qan/filter-menu/models/filter-group.model.ts
@@ -12,30 +12,7 @@ export class FilterGroupModel {
     } else {
       itemsArray = itemsGroup.name.map(item => new FilterModel(item));
     }
-    this.filterGroup = this.humanNamesForGroup(filterGroup);
+    this.filterGroup = filterGroup;
     this.items = itemsArray;
-  }
-
-  humanNamesForGroup(groupName) {
-    switch (groupName) {
-      case 'environment':
-        return 'Environment';
-      case 'cluster':
-        return 'Cluster';
-      case 'replication_set':
-        return 'Replication Set';
-      case 'd_database':
-        return 'Database';
-      case 'd_schema':
-        return 'Schema';
-      case 'd_server':
-        return 'Server';
-      case 'd_client_host':
-        return 'Client Host';
-      case 'd_username':
-        return 'User Name';
-      default:
-        return groupName
-    }
   }
 }
