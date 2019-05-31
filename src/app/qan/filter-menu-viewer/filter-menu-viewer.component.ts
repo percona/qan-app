f9c12251be62f22092a9c2cfd7da99a71f637018
Daria Lymanska <daria.lymanska@gmail.com>
2019-05-31 13:58:50 +0300

    PMM-4115 Update models

--- src/app/qan/filter-menu-viewer/filter-menu-viewer.component.ts
+++ src/app/qan/filter-menu-viewer/filter-menu-viewer.component.ts
@@ -3,13 +3,20 @@ import { GetProfileBody, QanProfileService } from '../profile/qan-profile.servic
 import { Subscription } from 'rxjs/internal/Subscription';
 import { FilterMenuService } from '../filter-menu/filter-menu.service';
 import { FiltersService } from '../../pmm-api-services/services/filters.service';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
+
+export interface FiltersGetParams {
+  main_metric_name: string,
+  period_start_from: string,
+  period_start_to: string
+}
 
 @Component({
   selector: 'app-filter-menu-viewer',
   templateUrl: './filter-menu-viewer.component.html',
   styleUrls: ['./filter-menu-viewer.component.css']
 })
+
 export class FilterMenuViewerComponent implements OnInit, OnDestroy {
 
   public currentParams: GetProfileBody;
@@ -25,19 +32,36 @@ export class FilterMenuViewerComponent implements OnInit, OnDestroy {
   ) {
     this.currentParams = this.qanProfileService.getProfileParams.getValue();
 
-    this.getFilters$ = this.filterService.Get(
-      {
-        period_start_from: this.currentParams.period_start_from,
-        period_start_to: this.currentParams.period_start_to
-      }
-    ).pipe(
-      map(response => this.filterMenuService.generateFilterGroup(response))
-    ).subscribe(
-      filters => {
-        this.filters = filters;
-        this.filterMenuService.updateAutocompleteFilters(filters)
-      }
-    );
+    this.getFilters$ = this.qanProfileService.getGroupBy.pipe(
+      switchMap(group_by => this.filterService.Get(
+        {
+          main_metric_name: group_by,
+          period_start_from: this.currentParams.period_start_from,
+          period_start_to: this.currentParams.period_start_to
+        }).pipe(
+          map(response => this.filterMenuService.generateFilterGroup(response))
+        )))
+      .subscribe(
+        filters => {
+          this.filters = filters;
+          this.filterMenuService.updateAutocompleteFilters(filters)
+        }
+      );
+
+    // this.getFilters$ = this.filterService.Get(
+    //   {
+    //     main_metric_name: group_by
+    //     period_start_from: this.currentParams.period_start_from,
+    //     period_start_to: this.currentParams.period_start_to
+    //   }
+    // ).pipe(
+    //   map(response => this.filterMenuService.generateFilterGroup(response))
+    // ).subscribe(
+    //   filters => {
+    //     this.filters = filters;
+    //     this.filterMenuService.updateAutocompleteFilters(filters)
+    //   }
+    // );
   }
 
   ngOnInit() {
