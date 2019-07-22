217b1bbd17a458cea1b5d5dfb33571902ccc626f
Daria Lymanska <daria.lymanska@percona.com>
2019-07-22 10:38:51 +0300

    PMM-2.0 Fix sorting order

--- src/app/qan/profile/qan-profile.service.ts
+++ src/app/qan/profile/qan-profile.service.ts
@@ -74,7 +74,7 @@ export class QanProfileService {
     labels: [],
     limit: 10,
     offset: 0,
-    order_by: 'load',
+    order_by: '-load',
     main_metric: 'load',
     period_start_from: this.setTimeRange('from'),
     period_start_to: this.setTimeRange('to')
