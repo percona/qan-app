271263a589dc204f0de465eaaf41ee9a5890decc
Daria Lymanska <daria.lymanska@gmail.com>
2019-07-25 12:42:16 +0300

    PMM-4151 Create component for action

---
+++ src/app/qan/profile-details/components/tables/components/table-action/table-action.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-action',
+  templateUrl: './table-action.component.html',
+  styleUrls: ['./table-action.component.css']
+})
+export class TableActionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
