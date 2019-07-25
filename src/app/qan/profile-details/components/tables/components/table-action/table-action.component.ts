81a0ea6154b68177677cc7f7e3d1438ad5d7aef7
Daria Lymanska <daria.lymanska@gmail.com>
2019-07-25 15:10:57 +0300

    PMM-4151 Add table action output on UI

--- src/app/qan/profile-details/components/tables/components/table-action/table-action.component.ts
+++ src/app/qan/profile-details/components/tables/components/table-action/table-action.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActionsService } from '../../../../../../pmm-api-services/services';
+import { catchError, startWith, switchMap } from 'rxjs/operators';
+import { interval, of, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-table-action',
@@ -6,10 +9,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table-action.component.css']
 })
 export class TableActionComponent implements OnInit {
+  @Input() tableName: string;
+  @Input() globalConfig: any;
 
-  constructor() { }
+  private table$: Subscription;
+
+  public tableDataOutput: any;
+  public error = '';
+  public isError = false;
+  public unsubscribe = false;
+
+  constructor(private actionsService: ActionsService) { }
 
   ngOnInit() {
+    this.startMySQLShowIndexAction(this.globalConfig, this.tableName);
+  }
+
+  private startMySQLShowIndexAction(value, tableName) {
+    this.table$ = this.actionsService.StartMySQLShowIndexAction({
+      service_id: value.service_id,
+      database: value.schema,
+      table_name: tableName
+    }
+    ).pipe(
+      catchError(error => {
+        error.error.done = true;
+        return of(error.error)
+      }),
+      switchMap((item) => !item.error ? this.getActionResult(item) : of(item))
+    ).subscribe(res => {
+      if (res.done) {
+        if (!res.error) {
+          this.error = '';
+          this.isError = false;
+          this.tableDataOutput = JSON.parse(res.output);
+          console.log('res - ', res);
+          console.log('createTableOutput - ', this.tableDataOutput);
+        } else {
+          this.error = res.error;
+          this.isError = true;
+        }
+        this.table$.unsubscribe();
+      }
+    });
+  }
+
+  private getActionResult(item) {
+    return interval(1000)
+      .pipe(
+        startWith(0),
+        switchMap(() => this.actionsService.GetAction({ action_id: item.action_id }))
+      );
   }
 
 }
