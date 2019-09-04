cc5ddcecccb84aa67bc4d1d1d1aac43b6b2788e4
Roma Novikov <roma.novikov@percona.com>
2019-09-04 18:24:26 +0300

    PMM-4477 Remote node monitoring. (#316)
    * PMM-4477 Remote node monitoring.
    
    * PMM-4477 Remove unnecessary line.
    
    * PMM-4477 Fix linter.
    
    * PMM-4477 Add query source select.
    
    * PMM-4477 Always add new node.
    
    * PMM-4477 Remote node monitoring for Mongo, PostgreSQL and ProxySQL.
    
    * PMM-4477 Remote node monitoring for Mongo, PostgreSQL and ProxySQL.
    
    * Fix conflicts.
    
    * Fix conflicts.

--- src/app/add-remote-instances/add-remote-instance.component.ts
+++ src/app/add-remote-instances/add-remote-instance.component.ts
@@ -1,24 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { AddRemoteInstanceService, RemoteInstanceCredentials } from './add-remote-instance.service'
+import { Component, NgModule, OnInit } from '@angular/core';
+import {
+  AddMySQLCredentials,
+  AddPostgreSQLCredentials,
+  AddNodeParams,
+  AddRemoteInstanceService,
+  BaseCredentials,
+  NodeRemote,
+  AddMongoDBCredentials,
+  AddProxySQLCredentials
+} from './add-remote-instance.service'
 import { environment } from '../environment';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { NgSelectModule } from '@ng-select/ng-select';
+
+@NgModule({
+  imports: [NgSelectModule]
+})
 
 @Component({
-  selector: 'app-add-remote-postgres',
+  selector: 'app-add-remote-instance',
   templateUrl: './add-remote-instance.component.html',
   styleUrls: ['./add-remote-instance.component.scss']
 })
 export class AddRemoteInstanceComponent implements OnInit {
 
-  remoteInstanceCredentials = {} as RemoteInstanceCredentials;
+  remoteInstanceCredentials = {} as BaseCredentials;
+  nodes = Array<NodeRemote>();
   errorMessage: string;
   isDemo = false;
   isLoading = false;
   isSubmitted = false;
   instanceType: string;
   currentUrl: string;
+  customLabels: string;
+  defaultPort = 3306;
 
-  constructor(public addRemoteInstanceService: AddRemoteInstanceService, private router: Router) {
+  constructor(public addRemoteInstanceService: AddRemoteInstanceService,
+    private router: Router
+  ) {
     this.isDemo = environment.demoHosts.includes(location.hostname);
     this.currentUrl = this.router.url;
   }
@@ -26,36 +46,80 @@ export class AddRemoteInstanceComponent implements OnInit {
   async ngOnInit() {
     this.errorMessage = '';
     this.isLoading = false;
-    this.instanceType =
-      this.addRemoteInstanceService.checkInstanceType(this.currentUrl) === 'postgresql' ? 'PostgreSQL' : 'MySQL';
+    switch (this.addRemoteInstanceService.checkInstanceType(this.currentUrl)) {
+      case 'postgresql':
+        this.instanceType = 'PostgreSQL';
+        this.remoteInstanceCredentials = {} as AddPostgreSQLCredentials;
+        this.defaultPort = 5432;
+        break;
+      case 'mysql':
+        this.instanceType = 'MySQL';
+        this.remoteInstanceCredentials = {} as AddMySQLCredentials;
+        this.defaultPort = 3306;
+        break;
+      case 'mongodb':
+        this.instanceType = 'MongoDB';
+        this.remoteInstanceCredentials = {} as AddMongoDBCredentials;
+        this.defaultPort = 27017;
+        break;
+      case 'proxysql':
+        this.instanceType = 'ProxySQL';
+        this.remoteInstanceCredentials = {} as AddProxySQLCredentials;
+        this.defaultPort = 6032;
+        break;
+    }
   }
 
   async onSubmit(form) {
     const currentUrl = `${window.parent.location}`;
-    const newURL = currentUrl.split('/graph/d/').shift() + '/graph/d/pmm-list/';
+    const newURL = currentUrl.split('/graph/d/').shift() + '/graph/d/pmm-inventory/';
 
     this.errorMessage = '';
     this.isSubmitted = true;
-    if (!form.valid) { return; }
+    if (!form.valid) {
+      return;
+    }
     this.isLoading = true;
 
-    if (this.remoteInstanceCredentials.name === undefined || this.remoteInstanceCredentials.name === '') {
-      this.remoteInstanceCredentials.name = this.remoteInstanceCredentials.address; // set default value for name (like address)
+    if (this.remoteInstanceCredentials.service_name === undefined || this.remoteInstanceCredentials.service_name === '') {
+      this.remoteInstanceCredentials.service_name = this.remoteInstanceCredentials.address; // set default value for name (like address)
     }
 
-    if (this.remoteInstanceCredentials.port === undefined || this.remoteInstanceCredentials.port === '') {
-      this.remoteInstanceCredentials.port = this.instanceType === 'PostgreSQL' ? '5432' : '3306'; // set default value for port
+    if (this.remoteInstanceCredentials.add_node === undefined) {
+      this.remoteInstanceCredentials.add_node = {
+        node_name: this.remoteInstanceCredentials.service_name,
+        node_type: 'REMOTE_NODE'
+      } as AddNodeParams;
     }
 
-    try {
-      const res = await this.addRemoteInstanceService.enable(this.remoteInstanceCredentials, this.currentUrl)
-        .then(() => {
-          window.parent.location.assign(newURL);
-        });
-
-    } catch (err) {
-      this.errorMessage = err.json().error;
+    if (this.remoteInstanceCredentials.port === undefined || this.remoteInstanceCredentials.port === 0) {
+      this.remoteInstanceCredentials.port = this.defaultPort; // set default value for port
     }
-    this.isLoading = false;
+    if (this.remoteInstanceCredentials.pmm_agent_id === undefined || this.remoteInstanceCredentials.pmm_agent_id === '') {
+      this.remoteInstanceCredentials.pmm_agent_id = 'pmm-server'; // set default value for pmm agent id
+    }
+
+    if (this.customLabels !== undefined) {
+      this.remoteInstanceCredentials.custom_labels = this.extractCustomLabels(this.customLabels);
+    }
+
+    const addObservable: Observable<Object> = this.addRemoteInstanceService.addService(this.remoteInstanceCredentials, this.currentUrl);
+    addObservable.subscribe(value => {
+      this.isLoading = false;
+      window.parent.location.assign(newURL);
+    }, err => {
+      this.errorMessage = err.error.error;
+      this.isLoading = false;
+    })
+  }
+
+  private extractCustomLabels(rawCustomLabels) {
+    const labels = {} as { [key: string]: string };
+    const labelPairs = rawCustomLabels.split('\n');
+    labelPairs.map(pair => {
+      const [key, value] = pair.split(':');
+      labels[key] = value
+    });
+    return labels;
   }
 }
--- src/app/add-remote-instances/add-remote-instance.service.ts
+++ src/app/add-remote-instances/add-remote-instance.service.ts
@@ -1,12 +1,113 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { MySQLService } from '../pmm-api-services/services/my-sql.service';
+import { PostgreSQLService } from '../pmm-api-services/services/postgre-sql.service';
+import { Observable } from 'rxjs';
+import { MongoDBService } from '../pmm-api-services/services/mongo-db.service';
+import { ProxySQLService } from '../pmm-api-services/services/proxy-sql.service';
 
-export interface RemoteInstanceCredentials {
-  address: string;
-  name: string;
-  port: string;
-  username: string;
-  password: string;
+export interface AddNodeParams {
+  node_name: string
+  node_type: 'NODE_TYPE_INVALID' | 'GENERIC_NODE' | 'CONTAINER_NODE' | 'REMOTE_NODE' | 'REMOTE_AMAZON_RDS_NODE'
+  machine_id?: string
+  distro?: string
+  container_id?: string
+  container_name?: string
+  node_model?: string
+  region?: string
+  az?: string
+  custom_labels?: { [key: string]: string }
+}
+
+export interface BaseCredentials {
+  node_id?: string
+  add_node?: AddNodeParams
+  address?: string
+  service_name?: string
+  port?: number
+  username?: string
+  password?: string
+  environment?: string
+  replication_set?: string
+  cluster?: string
+  custom_labels?: { [key: string]: string }
+  pmm_agent_id?: string
+  skip_connection_check?: boolean
+}
+
+export interface AddMySQLCredentials extends BaseCredentials {
+  qan_mysql_perfschema?: boolean
+  qan_mysql_slowlog?: boolean
+}
+
+export interface AddPostgreSQLCredentials extends BaseCredentials {
+  qan_postgresql_pgstatements_agent?: boolean
+}
+
+export interface AddMongoDBCredentials extends BaseCredentials {
+  qan_mongodb_profiler?: boolean
+}
+
+// tslint:disable-next-line:no-empty-interface
+export interface AddProxySQLCredentials extends BaseCredentials { }
+
+export interface NodeContainer {
+  address?: string
+  az?: string
+  container_id?: string
+  container_name?: string
+  custom_labels?: { [key: string]: string }
+  machine_id?: string
+  node_id?: string
+  node_model?: string
+  node_name?: string
+  region?: string
+}
+
+export interface NodeGeneric {
+  address?: string
+  az?: string
+  custom_labels?: { [key: string]: string }
+  distro?: string
+  machine_id?: string
+  node_id?: string
+  node_model?: string
+  node_name?: string
+  region?: string
+}
+
+export interface NodeRemote {
+  address?: string
+  az?: string
+  custom_labels?: { [key: string]: string }
+  node_id?: string
+  node_name?: string
+  region?: string
+}
+
+export interface NodeRemoteAmazon {
+  custom_labels?: { [key: string]: string }
+  instance?: string
+  node_id?: string
+  node_name?: string
+  region?: string
+}
+
+export interface Node {
+  container?: Array<NodeContainer>
+  generic?: Array<NodeGeneric>
+  remote?: Array<NodeRemote>
+  remote_amazon_rds?: Array<NodeRemoteAmazon>
+}
+
+export class NodeType {
+  value: string;
+  label: string;
+
+  constructor(value, label) {
+    this.value = value;
+    this.label = label;
+  }
 }
 
 export interface RemoteInstanceNode {
@@ -34,30 +135,34 @@ export class AddRemoteInstanceService {
   private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
   instanceUrlPart: string;
 
-  constructor(private httpClient: HttpClient) {
+  constructor(
+    private mySQLService: MySQLService,
+    private postgreSQLService: PostgreSQLService,
+    private mongodbService: MongoDBService,
+    private proxySQLService: ProxySQLService
+  ) {
   }
-
-  async enable(remoteInstanceCredentials: RemoteInstanceCredentials, currentUrl): Promise<{}> {
-    this.instanceUrlPart = this.checkInstanceType(currentUrl);
-
-    const url = `/managed/v0/${this.instanceUrlPart}`;
-    const data = {
-      address: remoteInstanceCredentials.address,
-      name: remoteInstanceCredentials.name,
-      port: remoteInstanceCredentials.port,
-      password: remoteInstanceCredentials.password,
-      username: remoteInstanceCredentials.username
-    };
-    return await this.httpClient
-      .post(url, data, { headers: this.httpHeaders })
-      .toPromise()
-  }
-
   /**
    * Returns type of remote instance
    * @param currentUrl current page url
    */
   checkInstanceType(currentUrl) {
-    return currentUrl === '/add-remote-postgres' ? 'postgresql' : 'mysql';
+    return currentUrl.replace('/add-remote-', '');
+  }
+
+  addService(remoteInstanceCredentials: BaseCredentials, currentUrl: string) {
+    const instanceType = this.checkInstanceType(currentUrl);
+    switch (instanceType) {
+      case 'mysql':
+        return this.mySQLService.AddMySQL(remoteInstanceCredentials);
+      case 'postgresql':
+        return this.postgreSQLService.AddPostgreSQL(remoteInstanceCredentials);
+      case 'mongodb':
+        return this.mongodbService.AddMongoDB(remoteInstanceCredentials);
+      case 'proxysql':
+        return this.proxySQLService.AddProxySQL(remoteInstanceCredentials);
+      default:
+        return Observable.throw({ error: { error: 'unsupported instance type' } });
+    }
   }
 }
