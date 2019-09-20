c856afa6d5ab669734bae1764ba7302c56719e9c
Nurlan Moldomurov <nurlan.moldomurov@percona.com>
2019-09-20 11:32:25 +0300

    Remove request instances list

--- src/app/core/services/instance.service.ts
+++ src/app/core/services/instance.service.ts
@@ -17,34 +17,7 @@ export interface Instance {
 
 @Injectable()
 export class InstanceService {
-  private instancesUrl = '/qan-api/instances?deleted=no';
   public dbServers: Array<Instance> = [];
   public dbServerMap: { [key: string]: Instance } = {};
-  constructor(private httpClient: HttpClient) { }
-
-  public getDBServers(): Promise<void | Instance[]> {
-    return this.httpClient.get(this.instancesUrl)
-      .toPromise()
-      .then((response: any) => {
-        const agents = response.filter(
-          (i: Instance) => i.Subsystem === 'agent'
-        ) as Instance[];
-
-        this.dbServers = (response.filter(
-          (i: Instance) => i.Subsystem === 'mysql' || i.Subsystem === 'mongo'
-        ) as Instance[]);
-
-        const agentsByParentUUID: { [key: string]: Instance } = {};
-        for (const agent of agents) {
-          agentsByParentUUID[agent.ParentUUID] = agent;
-        }
-
-        for (const srv of this.dbServers) {
-          this.dbServerMap[srv.Name] = srv;
-          this.dbServerMap[srv.Name].Agent = agentsByParentUUID[srv.ParentUUID];
-        }
-        return this.dbServers;
-      })
-      .catch(err => console.log(err));
-  }
+  constructor() { }
 }
