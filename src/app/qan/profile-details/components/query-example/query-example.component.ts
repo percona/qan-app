1c1073f9672b31b148b2627ad44c08d68d276f20
Roman Misyurin <roman.misyurin@percona.com>
2019-10-12 14:09:32 +0300

    feat: added JSON formatting fix: added words wrap in examples output

--- src/app/qan/profile-details/components/query-example/query-example.component.ts
+++ src/app/qan/profile-details/components/query-example/query-example.component.ts
@@ -1,43 +1,57 @@
-import { Component, Input } from '@angular/core';
-import * as vkbeautify from 'vkbeautify';
-import * as hljs from 'highlight.js';
+import { Component, Input } from "@angular/core";
+import * as vkbeautify from "vkbeautify";
+import * as hljs from "highlight.js";
+import sqlFormatter from "sql-formatter";
 
 @Component({
-  selector: 'app-query-example',
-  templateUrl: './query-example.component.html',
-  styleUrls: ['./query-example.component.css']
+  selector: "app-query-example",
+  templateUrl: "./query-example.component.html",
+  styleUrls: ["./query-example.component.css"]
 })
 export class QueryExampleComponent {
   @Input() exampleParam: any;
   @Input() beatify: boolean;
+  @Input() serviceType: string;
 
   public isCopied = false;
-  event = new Event('showSuccessNotification');
+  event = new Event("showSuccessNotification");
 
-  constructor() {
-  }
+  constructor() {}
 
   /**
    * Fix beautify dispalying text, will be delete after approve https://github.com/vkiryukhin/vkBeautify/pull/25
    * @param {string} text
    * @returns {string}
    */
+
   fixBeautifyText(text: string): string {
-    return vkbeautify.sql(text.toLowerCase()).replace('explain', 'EXPLAIN ').replace('  ', ' ');
+    return sqlFormatter.format(text.toLowerCase())
+      .replace("explain", "EXPLAIN ")
+      .replace("  ", " ");
   }
 
   highlightExampleQuery(exampleText) {
+    // TODO: add syntax highlighting
     if (this.beatify) {
-      exampleText = this.fixBeautifyText(exampleText);
+      switch (this.serviceType) {
+        case "mongodb":
+          return vkbeautify.json(exampleText, 2);
+          break;
+        case "SQL":
+          return this.fixBeautifyText(exampleText);
+          break;
+        default:
+          return this.fixBeautifyText(exampleText);
+      }
     }
-    return hljs.highlight('sql', exampleText).value;
+    return "";
   }
 
   showSuccessNotification() {
     this.isCopied = true;
     window.parent.document.dispatchEvent(this.event);
     setTimeout(() => {
-      this.isCopied = false
+      this.isCopied = false;
     }, 3000);
   }
 }
