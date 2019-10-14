aa3d2a995dd752d6cd8b789d4b73389af8b95e21
Roman Misyurin <roman.misyurin@percona.com>
2019-10-14 08:14:42 +0300

    lint: eslint issues fixed

--- src/app/qan/profile-details/components/details-table/details-table.component.ts
+++ src/app/qan/profile-details/components/details-table/details-table.component.ts
@@ -2,31 +2,32 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
-  EventEmitter, HostBinding,
+  EventEmitter,
+  HostBinding,
   OnInit,
   Output,
   QueryList,
   ViewChild,
   ViewChildren
-} from '@angular/core';
-import { catchError, map, switchMap } from 'rxjs/operators';
-import { of, Subscription } from 'rxjs';
-import { MetricModel } from '../../../profile-table/models/metric.model';
-import { Router } from '@angular/router';
-import { QanProfileService } from '../../../profile/qan-profile.service';
-import { ObjectDetailsService } from '../../../../pmm-api-services/services/object-details.service';
-import { DetailsSparklineModel } from '../../models/details-sparkline.model';
-import { ObjectDetails } from '../../../profile/interfaces/object-details.interface';
+} from "@angular/core";
+import { catchError, map, switchMap } from "rxjs/operators";
+import { of, Subscription } from "rxjs";
+import { MetricModel } from "../../../profile-table/models/metric.model";
+import { Router } from "@angular/router";
+import { QanProfileService } from "../../../profile/qan-profile.service";
+import { ObjectDetailsService } from "../../../../pmm-api-services/services/object-details.service";
+import { DetailsSparklineModel } from "../../models/details-sparkline.model";
+import { ObjectDetails } from "../../../profile/interfaces/object-details.interface";
 
 @Component({
-  selector: 'app-details-table',
-  templateUrl: './details-table.component.html',
-  styleUrls: ['./details-table.component.css']
+  selector: "app-details-table",
+  templateUrl: "./details-table.component.html",
+  styleUrls: ["./details-table.component.css"]
 })
 export class DetailsTableComponent implements OnInit, AfterViewInit {
-  @ViewChild('table', { static: true }) table: ElementRef;
-  @ViewChildren('detailsTableRows') tableRows: QueryList<any>;
-  @HostBinding('class.frame') isLoading = true;
+  @ViewChild("table", { static: true }) table: ElementRef;
+  @ViewChildren("detailsTableRows") tableRows: QueryList<any>;
+  @HostBinding("class.frame") isLoading = true;
   @Output() finishRender = new EventEmitter();
 
   protected dbName: string;
@@ -43,25 +44,30 @@ export class DetailsTableComponent implements OnInit, AfterViewInit {
   constructor(
     protected router: Router,
     protected qanProfileService: QanProfileService,
-    protected objectDetailsService: ObjectDetailsService,
+    protected objectDetailsService: ObjectDetailsService
   ) {
     this.isLoading = true;
-    this.details$ = this.qanProfileService.getProfileInfo.details.pipe(
-      switchMap(parsedParams => {
-        this.isLoading = true;
-        this.currentParams = parsedParams;
-        return this.getDetailsData(parsedParams);
-      }),
-    ).subscribe(response => {
-      this.details = response;
-      this.details.metrics = this.detailsTableOrder(response['metrics']);
-      this.queryCount = this.details.metrics.find(item => item.metricName === 'num_queries').stats['sum'];
-      this.isTotal = !this.currentParams.filter_by;
-      this.isLoading = false;
-    });
+    this.details$ = this.qanProfileService.getProfileInfo.details
+      .pipe(
+        switchMap(parsedParams => {
+          this.isLoading = true;
+          this.currentParams = parsedParams;
+          return this.getDetailsData(parsedParams);
+        })
+      )
+      .subscribe(response => {
+        this.details = response;
+        this.details.metrics = this.detailsTableOrder(response["metrics"]);
+        this.queryCount = this.details.metrics.find(
+          item => item.metricName === "num_queries"
+        ).stats["sum"];
+        this.isTotal = !this.currentParams.filter_by;
+        this.isLoading = false;
+      });
 
-    this.group_by$ = this.qanProfileService.getGroupBy
-      .subscribe(() => this.details.metrics = [])
+    this.group_by$ = this.qanProfileService.getGroupBy.subscribe(
+      () => (this.details.metrics = [])
+    );
   }
 
   ngOnInit() {
@@ -75,12 +81,12 @@ export class DetailsTableComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     this.tableRows.changes.subscribe(() => {
-      this.setLabelsHeight()
-    })
+      this.setLabelsHeight();
+    });
   }
 
   createSparklineModel(sparklines, name) {
-    return sparklines.map(item => new DetailsSparklineModel(item, name))
+    return sparklines.map(item => new DetailsSparklineModel(item, name));
   }
 
   setLabelsHeight() {
@@ -96,32 +102,57 @@ export class DetailsTableComponent implements OnInit, AfterViewInit {
     return this.objectDetailsService.GetMetrics(detailsParams).pipe(
       catchError(err => of({ metrics: [], sparkline: [] })),
       map(response => {
-        const withData = Object.entries(response.metrics ? response.metrics : response['totals'])
-          .filter(metricData => Object.keys(metricData[1]).length)
-          .filter(metricData => !(metricData[1]['cnt'] !== 0 && metricData[1]['sum'] === undefined))
-          .map(withDataItem => {
-            const sparklineData = this.createSparklineModel(response.sparkline, withDataItem[0]);
-            return new MetricModel(withDataItem, sparklineData)
-          }) || [];
-        const withDataTotals = Object.entries(response['totals'])
-          .filter(metricData => Object.keys(metricData[1]).length)
-          .map(withDataTotalsItem => new MetricModel(withDataTotalsItem))
-          .filter(totalsItem => withData
-            .some(metricsItem => metricsItem.metricName === totalsItem.metricName)) || [];
+        const withData =
+          Object.entries(
+            response.metrics ? response.metrics : response["totals"]
+          )
+            .filter(metricData => Object.keys(metricData[1]).length)
+            .filter(
+              metricData =>
+                !(
+                  metricData[1]["cnt"] !== 0 &&
+                  metricData[1]["sum"] === undefined
+                )
+            )
+            .map(withDataItem => {
+              const sparklineData = this.createSparklineModel(
+                response.sparkline,
+                withDataItem[0]
+              );
+              return new MetricModel(withDataItem, sparklineData);
+            }) || [];
+        const withDataTotals =
+          Object.entries(response["totals"])
+            .filter(metricData => Object.keys(metricData[1]).length)
+            .map(withDataTotalsItem => new MetricModel(withDataTotalsItem))
+            .filter(totalsItem =>
+              withData.some(
+                metricsItem => metricsItem.metricName === totalsItem.metricName
+              )
+            ) || [];
         return {
           metrics: withData,
           totals: withDataTotals
         };
       }),
-      catchError(err => of([])));
+      catchError(err => of([]))
+    );
   }
 
   sortDetails(a, b) {
-    const order =
-      ['num_queries', 'num_queries_with_errors', 'num_queries_with_warnings', 'query_time', 'lock_time', 'rows_sent', 'rows_examined', ''];
+    const order = [
+      "num_queries",
+      "num_queries_with_errors",
+      "num_queries_with_warnings",
+      "query_time",
+      "lock_time",
+      "rows_sent",
+      "rows_examined",
+      ""
+    ];
 
-    let indA = order.indexOf(a['metricName']);
-    let indB = order.indexOf(b['metricName']);
+    let indA = order.indexOf(a["metricName"]);
+    let indB = order.indexOf(b["metricName"]);
 
     if (indA === -1) {
       indA = order.length - 1;
